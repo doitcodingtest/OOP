8400c592480270dcae4215755d120e36d94016dd
doitcodingtest <108266421+doitcodingtest@users.noreply.github.com>
2025-03-17 14:04:03 +0900

    Update User.java
    yutube -> youtube 오타 변경

--- SOLID/DIP2/src/User.java
+++ SOLID/DIP2/src/User.java
@@ -1,6 +1,6 @@
 public class User {
     private MusicPlayer musicPlayer;
-    private YoutubePlayer yutubePlayer;
+    private YoutubePlayer youtubePlayer;
     private String type;
 
     public void setMusicPlayer(MusicPlayer musicPlayer) {
@@ -8,16 +8,16 @@ public class User {
         type = "music";
     }
 
-    public void setYutubePlayer(YoutubePlayer yutubePlayer) {
-        this.yutubePlayer = yutubePlayer;
-        type = "yutube";
+    public void setYoutubePlayer(YoutubePlayer youtubePlayer) {
+        this.youtubePlayer = youtubePlayer;
+        type = "youtube";
     }
 
     public void play() {
         if (type.equals("music")) {
             musicPlayer.play();
         } else {
-            yutubePlayer.play();
+            youtubePlayer.play();
         }
     }
 
