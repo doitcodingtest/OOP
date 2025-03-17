076af13ea7a3984a8e26a9883183ff8e21e1abf7
doitcodingtest <108266421+doitcodingtest@users.noreply.github.com>
2025-03-17 13:14:26 +0900

    Update MedicineClient.java
    주석 수정

--- OOP_Feature/Encapsulation/src/MedicineClient.java
+++ OOP_Feature/Encapsulation/src/MedicineClient.java
@@ -1,10 +1,10 @@
 public class MedicineClient {
     public static void main(String[] args) {
         Medicine A = new MedicineA();
-       // ((MedicineA) A).count = 10; 직접 count를 접근 할 수 없습니다. => 데이터 보호
+       // ((MedicineA) A).count = 10; 직접 count에 접근 불가 => 데이터 보호
         A.add();
         A.add();
-        A.add(); //client에서는 add를 하였지만 은닉된 내부 로직에 의해 약은 더 추가 되지 않습니다 => 데이터 은닉
+        A.add(); // 내부 로직에 의해 약은 더 이상 추가 불가  => 데이터 은닉
         A.eat();
     }
 }
